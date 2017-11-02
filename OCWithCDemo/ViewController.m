76911c69304e74613419e2161c59b98b23ac0742
cwb <2184682981@qq.com>
2017-11-02 17:08:30 +0800

    Update ViewController.m

--- OCWithCDemo/ViewController.m
+++ OCWithCDemo/ViewController.m
@@ -17,16 +17,6 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     test();
-    
-    
-    NSLog(@"s");
-    
-    
-    
-    
-    NSLog(@"s");
-    NSLog(@"s");
-    NSLog(@"s");
 
 }
 
